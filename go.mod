2108a24e99b504a2a90f45243caf5f8b83bebc87
JamesClonk <jamesclonk@jamesclonk.ch>
2021-12-26 14:08:29 +0100

    wip, add rudimentary bot response

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/JamesClonk/iRdiscordbot
 go 1.16
 
 require (
+	github.com/bwmarrin/discordgo v0.23.2
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
 )
--- main.go
+++ main.go
@@ -1,17 +1,71 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/JamesClonk/iRdiscordbot/env"
 	"github.com/JamesClonk/iRdiscordbot/log"
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-	port := env.Get("PORT", "8080")
 	level := env.Get("LOG_LEVEL", "info")
-	username := env.MustGet("AUTH_USERNAME")
-	//password := env.MustGet("AUTH_PASSWORD")
-
-	log.Infoln("port:", port)
+	token := env.MustGet("BOT_TOKEN")
 	log.Infoln("log level:", level)
-	log.Infoln("auth username:", username)
+
+	// create Discord session using bot token
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Errorf("error creating Discord session: %v", err)
+		return
+	}
+
+	dg.AddHandler(onMessageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// connect
+	if err := dg.Open(); err != nil {
+		log.Errorf("error opening connection: %v", err)
+		return
+	}
+
+	log.Infoln("iRdiscordbot is running...")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill, syscall.SIGKILL)
+	<-sc
+
+	// disconnect
+	dg.Close()
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// ignore messages by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// if m.Content == "!summary" {
+	// 	if _, err := s.ChannelMessageSend(m.ChannelID, "https://irvisualizer.jamesclonk.io/season/3492/summary.png?team=TNT%20Racing"); err != nil {
+	// 		log.Errorf("error sending message: %v", err)
+	// 		return
+	// 	}
+	// }
+	if m.Content == "!summary" {
+		embed := discordgo.MessageEmbed{
+			Title:       "Radical Racing Challenge - Driver Summary",
+			Description: "Shows driver summary data for the whole season",
+			Type:        discordgo.EmbedTypeImage,
+			Image: &discordgo.MessageEmbedImage{
+				URL: fmt.Sprintf("https://irvisualizer.jamesclonk.io/season/3492/summary.png?team=TNT%%20Racing&cb=%d", time.Now().UnixNano()/1000/1000/1000),
+			},
+		}
+		if _, err := s.ChannelMessageSendEmbed(m.ChannelID, &embed); err != nil {
+			log.Errorf("error sending message: %v", err)
+			return
+		}
+	}
 }
